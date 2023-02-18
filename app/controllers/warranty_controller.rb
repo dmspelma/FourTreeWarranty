1067ff12a9f6e7c7ce17176f13612da26eb61276
Drew Spelman <drewspelman@gmail.com>
2023-02-17 22:40:34 -0800

    Update routes, show, index for new & create controller methods

--- app/controllers/warranty_controller.rb
+++ app/controllers/warranty_controller.rb
@@ -1,5 +1,29 @@
 class WarrantyController < ApplicationController
   def index
-    @test = 'surprise'
+    @warranties = Warranty.all
+  end
+
+  def show
+    @warranty = Warranty.find(params[:id])
+  end
+
+  def new
+    @warranty = Warranty.new
+  end
+
+  def create
+    @warranty = Warranty.new(warranty_params)
+
+    if @warranty.save
+      redirect_to @warranty
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def warranty_params
+    params.require(:warranty).permit(:warranty_name, :warranty_company, :warranty_start_date, :warranty_end_date)
   end
 end
--- app/models/warranty.rb
+++ app/models/warranty.rb
@@ -1,2 +1,6 @@
 class Warranty < ApplicationRecord
+    validates :warranty_name, presence: true
+    validates :warranty_company, presence: true
+    validates :warranty_start_date, presence: true
+    validates :warranty_end_date, presence: true
 end
