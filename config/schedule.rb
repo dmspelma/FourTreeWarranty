012708da6facb8402c01c6f972a59f67d2339d95
Drew Spelman <drewspelman@gmail.com>
2023-04-22 16:36:00 -0700

    Add job to expire warranties if they are older than current day. Schedule job to run once a day

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+# NOTE: The times below are actually utc time.
+
 # Fire email notifying users of warranties soon to expire
 every 1.day, at: '9am' do
   WarrantyExpiringJob.perform_now
+end
+
+# Find and Expire warranties every day
+every 1.day, at: '8:30am' do
+  ExpireWarrantyJob.perform_now
 end
